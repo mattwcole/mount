d7973f067f8d36f3d8bf026c0b1b8392d679afad
Matt Cole <matt-cole@hotmail.co.uk>
2015-04-04 16:25:51 +0100

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+name             'mount'
+maintainer       'Matt Cole'
+maintainer_email 'matt-cole@hotmail.co.uk'
+license          'Apache 2.0'
+description      'Mounts drives to machines'
+version          '0.1.0'
---
+++ spec/unit/recipes/default_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe 'mount::default' do
+  context 'When all attributes are default, on an unspecified platform' do
+    let(:chef_run) do
+      runner = ChefSpec::ServerRunner.new
+      runner.converge(described_recipe)
+    end
+
+    it 'converges successfully' do
+      chef_run # This should not raise an error
+    end
+  end
+end
